456b53eb2831fe7a1045c5b8eb48d76ccfa6f6d9
David <cemi201913@gmail.com>
2025-07-17 19:41:07 +0200

    cambios en top 100 peliculas y en top 10 actres

--- 100_peliculas_mejor_valoradas.py
+++ 100_peliculas_mejor_valoradas.py
@@ -60,12 +60,9 @@ def obtener_top_peliculas(api_key, paginas=5, preset=None, **presets):
 # Tu API key
 api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjJkNjI2ZmUzYjIyNjA5M2M1MzE3MTE2YTE1Yzc4NiIsIm5iZiI6MTc1MjA2OTAzNi44NTIsInN1YiI6IjY4NmU3M2FjYTcyMmQzODk0YjEwNDYzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VbNiPXVBiDP8jr7KPcJg0YXkttw5T7nJqnkgNVPwKr8"
 
-<<<<<<< HEAD
 # Ejemplo de uso
 peliculas = obtener_top_peliculas(api_key, paginas=5, preset="esp")
 
 
 print(peliculas)
-=======
 obtener_top_peliculas(api_key, paginas=5,preset="esp")
->>>>>>> 5c4f6c00c191730af3ba6c3f28abdca79bcf0760
